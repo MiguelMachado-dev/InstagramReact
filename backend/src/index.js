3a5ecd6505d6bf5d3b0ce551d1e6980605aa8041
Miguel Machado <miiguellmp@gmail.com>
2019-06-11 10:13:35 -0300

    using cors, socketio and static url path to get imgsrc

---
+++ backend/src/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+mongoose.connect(
+  "mongodb+srv://semana:semana@cluster0-z6pxr.mongodb.net/test?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true
+  }
+);
+
+// tudo o que vier depois do app.use terá acesso ao meu req.io
+app.use((req, res, next) => {
+  req.io = io;
+
+  next();
+});
+
+app.use(cors());
+
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "uploads", "resized"))
+);
+
+app.use(require("./routes"));
+
+server.listen(3333);
