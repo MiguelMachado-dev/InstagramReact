cc64e741cd8288b569e2fb911b498515c16bcab9
Miguel Machado <miiguellmp@gmail.com>
2019-06-12 10:37:53 -0300

    finishing frontend web

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import api from "../services/api";
+import io from "socket.io-client";
 
 import "./Feed.css";
 
@@ -14,10 +15,31 @@ class Feed extends Component {
   };
 
   async componentDidMount() {
+    this.registerToSocket();
     const response = await api.get("posts");
 
     this.setState({ feed: response.data });
   }
+
+  registerToSocket = () => {
+    const socket = io("http://localhost:3333");
+
+    socket.on("post", newPost => {
+      this.setState({ feed: [newPost, ...this.state.feed] });
+    });
+
+    socket.on("like", likedPost => {
+      this.setState({
+        feed: this.state.feed.map(post =>
+          post._id === likedPost._id ? likedPost : post
+        )
+      });
+    });
+  };
+
+  handleLike = id => {
+    api.post(`/posts/${id}/like`);
+  };
 
   render() {
     return (
@@ -37,7 +59,9 @@ class Feed extends Component {
 
             <footer>
               <div className="actions">
-                <img src={like} alt="" />
+                <button type="button" onClick={() => this.handleLike(post._id)}>
+                  <img src={like} alt="" />
+                </button>
                 <img src={comment} alt="" />
                 <img src={send} alt="" />
               </div>
